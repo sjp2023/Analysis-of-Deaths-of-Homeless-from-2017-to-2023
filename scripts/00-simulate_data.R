aa9b85a13433dfc3b5230d21126a24c3eb98cee8
sjp2023 <150992530+sjp2023@users.noreply.github.com>
2024-01-22 15:12:47 -0500

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,1 +1,16 @@
+#### Preamble ####
+# Purpose: Simulating the data from Deaths of People Experiencing Homelessness
+# Author: Sky Suh
+# Date: 20 January 2024
+# Contact: sky.suh@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
 
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Download data ####
