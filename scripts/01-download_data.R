dc91ca6caff38c77d005be08c1608e09a01622c8
sjp2023 <150992530+sjp2023@users.noreply.github.com>
2024-01-23 22:27:12 -0500

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,49 +1,20 @@
 library(tidyverse)
-library(janitor)
-library(dplyr)
+library(opendatatoronto)
 
-raw_cause_data <- read_csv("raw_cause_data.csv")
-raw_month_data <- read_csv("raw_month_data.csv")
-raw_demographic_data <- read_csv("raw_demographic_data.csv")
+list<-list_package_resources("https://open.toronto.ca/dataset/deaths-of-people-experiencing-homelessness/")
 
+raw_cause_data <- 
+  list[6,] %>%
+  get_resource
 
-cleaned_cause_data <-
-  clean_names(raw_cause_data)
+raw_month_data <- 
+  list[2,] %>%
+  get_resource
 
-cleaned_month_data <-
-  clean_names(raw_month_data)
+raw_demographic_data <- 
+  list[10,] %>%
+  get_resource()
 
-cleaned_demographic_data <-
-  clean_names(raw_demographic_data)
-
-data <- read.csv("raw_cause_data.csv")
-
-selected_columns <- c("X.id", "Year.of.death", "Cause_of_death", "Count")
-raw_cause_data <- select(raw_cause_data, "X_id", "Year.of.death", "Cause_of_death", "Count")
-
-raw_cause_data_no_missing <- raw_cause_data %>% na.omit()
-
-raw_cause_data_unique <- raw_cause_data_no_missing %>% distinct()
-
-threshold <- 3
-raw_cause_data_no_outliers <- raw_cause_data_unique 
-
-raw_cause_data_no_outliers$Count <- as.numeric(raw_cause_data_no_outliers$Count)
-
-raw_cause_data_no_outliers$new_variable <- raw_cause_data_no_outliers$Year.of.death * 2
-
-summary(raw_cause_data_no_outliers)
-
-
-
-cleaned_cause_data2 <- select(cleaned_cause_data, x_id,year_of_death,cause_of_death, count)
-
-cleaned_cause_data3 <- select(cleaned_cause_data, year_of_death,count)
-
-
-
-write_csv(cleaned_cause_data,"cleaned_cause_data.csv")
-write_csv(cleaned_month_data,"cleaned_month_data.csv")
-write_csv(cleaned_demographic_data,"cleaned_demographic_data.csv")
-write.csv(cleaned_cause_data2, "cleaned_cause_data2.csv", row.names = FALSE)
-write_csv(cleaned_cause_data3,"cleaned_cause_data3.csv")
+write_csv(raw_cause_data,"raw_cause_data.csv")
+write_csv(raw_month_data,"raw_month_data.csv")
+write_csv(raw_demographic_data,"raw_demographic_data.csv")
