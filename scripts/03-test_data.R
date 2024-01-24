1a2af017207de09bac883394751adf7b445228f4
sjp2023 <150992530+sjp2023@users.noreply.github.com>
2024-01-24 00:34:09 -0500

    Update 03-test_data.R

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -5,9 +5,9 @@
 # Contact: sky.suh@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
-# 00-simulate_data.R
-# 01-download_data.R
-# 02-data_cleaning.R
+  # 00-simulate_data.R
+  # 01-download_data.R
+  # 02-data_cleaning.R
 #Code referenced from:https://tellingstorieswithdata.com/02-drinking_from_a_fire_hose.html
 # Data sets:
 ## - https://open.toronto.ca/dataset/deaths-of-people-experiencing-homelessness/
@@ -23,6 +23,10 @@ data$cleaned_cause_data |> max() == 2023
 data$cleaned_month_data |> min() >= "January"
 data$cleaned_month_data |> max() <= "December"
 data$cleaned_month_datah |> class() == "character"
+
+cleaned_demographic_data$age_group |>
+  unique() |>
+  length() == 5
 
 cleaned_demographic_data$gender |>
   unique() |>
